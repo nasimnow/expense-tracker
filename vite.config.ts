cb6eee112ff740df3d93aa60a271d7a40b868969
hadi-a-azeez <muhammednasimda@gmail.com>
2022-07-01 19:02:28 +0400

    added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,5 @@ export default defineConfig({
         plugins: ["babel-plugin-macros", "babel-plugin-styled-components"],
       },
     }),
-    VitePWA({ registerType: "autoUpdate" }),
   ],
 });
